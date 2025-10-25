09ae319ec3a2261ecaf798d9cd81ffb36b505d27
anonymous-colonel <robensonauguste524@gmail.com>
2025-10-25 01:03:29 -0500

    Ajout/Mise à jour de scripts/003_create_admin_accounts.sql

---
+++ scripts/003_create_admin_accounts.sql
@@ -0,0 +1,9 @@
+-- Create default admin and subadmin accounts
+-- Note: These will be created through the application signup flow with proper password hashing
+
+-- This script is for reference only
+-- The actual admin accounts will be created via the signup API with:
+-- Super Admin: username=admin, password=kerventz2005
+-- Sub Admin: username=subadmin, password=admin1234
+
+-- After signup, we'll update their roles manually or through admin panel
